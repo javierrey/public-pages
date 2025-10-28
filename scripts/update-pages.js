71a9aacf1115cc805618b2e80295da5c99a74b6e
U-JRC0\javier <javier.rey.eu@gmail.com>
2025-10-28 12:26:54 +0000

    improve update-pages

--- scripts/update-pages.js
+++ scripts/update-pages.js
@@ -6,22 +6,14 @@ import fs from 'fs';
 import { spawnSync } from 'child_process';
 
 const argv = process.argv.slice(2);
-const base = '.';
-const log = console.log; // eslint-disable-line
 const fsP = fs.promises;
+const log = console.log; // eslint-disable-line
+const base = '.';
 
-const apps = argv.map((app) => app.replace(/^['"`]/, '').replace(/['"`]$/, ''));
+const apps = argv.map((app) => app.replace(/^['"`]|['"`]$/g, ''));
 const dir = 'public';
 
 log(spawnSync('echo', [`update-pages.js for ${dir}: [${apps}]`]).stdout.toString());
-
-apps.forEach((app) => {
-  const src = `${base}/apps/${app}/src/${dir}`;
-  if (!fs.existsSync(src)) { return; }
-  const dest = `${base}/${dir}/${app}`;
-  spawnSync('mkdir', ['-p', dest]);
-  spawnSync('cp', ['-R', `${src}/.`, dest]);
-});
 
 /** @param {string} app */
 const copyGithubPages = (app) => {
@@ -33,10 +25,7 @@ const copyGithubPages = (app) => {
   return 0;
 };
 
-/** 
- * @param {string[]} apps
- * @param {string} title
- */
+/** @param {string[]} apps, @param {string} title */
 const copyAndIndex = (apps, title) => {
     let html = `<html><head><title>${title}</title></head><body><h3>${title}</h3><ul>`;
     apps.forEach((app) => {
